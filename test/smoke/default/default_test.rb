989c594633e966257c8e1b127d83a982aeda7525
Naren Iyer <naren.iyer@hotmail.com>
2017-09-19 16:13:14 +0100

    initial commit

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -12,7 +12,25 @@ unless os.windows?
   end
 end
 
+describe package 'nginx' do
+	it { should be_installed }
+end
+
+describe service 'nginx' do 
+	it {should be_running }
+	it {should be_enabled }
+end
+
 # This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe port(80) do
+  it { should be_listening }
+end
+
+describe package 'nodejs' do
+	it {should be_installed}
+	its ('version') {should match /6\../}
+end
+
+describe npm('pm2') do 
+	it {should be_installed }
 end
